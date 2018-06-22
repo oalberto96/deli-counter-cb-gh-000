aebe17c0693f8e00b512b4a2222c81b269f8b9f4
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 21:01:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,10 @@ katz_deli = []
 def line
   if katz_deli.size < 1
     puts "The line is currently empty."
+  end
 end
 
 def take_a_number(katz_deli, new_person)
   katz_deli.push(new_person)
   puts katz_deli.index + 1
+end
