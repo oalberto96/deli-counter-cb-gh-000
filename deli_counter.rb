426cbbefb4d3a902b6ad7988781f6924dbe99e90
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 21:14:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,9 @@ def take_a_number(katz_deli, new_person)
   katz_deli.push(new_person)
   puts "Welcome, #{new_person}. You are number #{katz_deli.size} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.size > 0
+    
+  end
+end
